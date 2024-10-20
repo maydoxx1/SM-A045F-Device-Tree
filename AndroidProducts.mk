8c05ecc92142534e36fff9385aa79d7b1e899753
Maydo Al-Dosery <maydohelp@gmail.com>
2024-10-20 21:39:07 +0300

    SM-A045F Device Tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_a04.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a04-user \
+    lineage_a04-userdebug \
+    lineage_a04-eng
